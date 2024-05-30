f9d7b7810dd68a2135d59f72a1d8d2f6340ba1d9
keoalmeida <keo.almeida@totvs.com.br>
2024-05-29 21:08:35 -0300

    gerenciamento de tarefas não está pronto, retomar daqui

---
+++ Dia5.GerenciadorDeTarefasComPrioridades/GerenciadorDeTarefasComPrioridades/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program{
+    static void Main (string[] args)
+    {
+        var listaDeTarefas = new string[3];
+        
+        Console.Clear();
+
+        Console.WriteLine("  GERENCIADOR DE TAREFAS  ");
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("Dê um título para tarefa:");
+        string titulo = Console.ReadLine();
+        Console.WriteLine("Descreva a tarefa:");
+        string tarefa = Console.ReadLine();
+        Console.WriteLine("Qual a prioridade?");
+        Console.WriteLine("1- Se não fizer, vai dar ruim");
+        Console.WriteLine("2- Dá pra fazer depois");
+        Console.WriteLine("3- Pode procrastinar");
+        string prioridade = Console.ReadLine();
+
+        listaDeTarefas[0] = titulo;
+        listaDeTarefas[1] = tarefa;
+        listaDeTarefas[2] = prioridade;
+
+        for (int i = 0; i < listaDeTarefas.Length; i++)
+        {
+            Console.WriteLine(listaDeTarefas[i]);
+        }
+    }
+}
